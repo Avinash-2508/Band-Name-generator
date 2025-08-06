859612b4a8e492bc0d64927c1f3791d4f629e8d1
Avinash <avinash.ponneboina@gmail.com>
2025-08-06 15:25:16 +0530

    Add a dynamic year to the footer.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
  
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
+  //Step 1 - Make the get route work and render the index.ejs file
   res.render("index.ejs")
 });
 
